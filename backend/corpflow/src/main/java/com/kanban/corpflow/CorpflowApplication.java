7bc7022dbf9776947f52c49fe81b7590ba2ad5ec
ArthLDP <arthurlourencodp@gmail.com>
2026-03-02 14:30:58 -0300

    Add backend spring application

---
+++ backend/corpflow/src/main/java/com/kanban/corpflow/CorpflowApplication.java
@@ -0,0 +1,13 @@
+package com.kanban.corpflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorpflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorpflowApplication.class, args);
+	}
+
+}
---
+++ backend/corpflow/src/test/java/com/kanban/corpflow/CorpflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanban.corpflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorpflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
